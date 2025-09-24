0cb07051a0c0cd93334f20224791d99ccbb844fb
thinh1598 <kejo1598@gmail.com>
2025-09-25 00:34:03 +0700

    hw1 introduction

---
+++ HW1/Introduction.java
@@ -0,0 +1,27 @@
+package Introduction;
+
+public class Introduction {
+
+	public static void main(String[] args) {
+		System.out.println("Nguyen Phuc Thinh\t24021634\tusername github:thinh1598\tkejo1598@gmail.com\t"
+				+ ""
+				+ "9 bottles of beer on the wall, 9 bottles of beer.\n"
+				+ "\n"
+				+ "Take one down, pass it around, \n"
+				+ "\n"
+				+ "8 bottles of beer on the wall, 8 bottles of beer.\n"
+				+ "\n"
+				+ "Take one down, pass it around,\n"
+				+ "\n"
+				+ "...\n"
+				+ "\n"
+				+ "1 bottle of beer on the wall, 1 bottle of beer.\n"
+				+ "\n"
+				+ "Take one down, pass it around,\n"
+				+ "\n"
+				+ "No more bottles of beer on the wall. ");
+		
+
+	}
+
+}
